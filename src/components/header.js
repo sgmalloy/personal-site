c5351be4bd79a8af2d9270ba3879c7cc0845b208
SHANE MALLOY <shanemalloy@SHANEs-MacBook-Pro-2.local>
2020-03-07 14:09:13 -0700

    second commit

--- src/components/header.js
+++ src/components/header.js
@@ -16,6 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
+      <img src="../images/gatsby-icon.png"></img>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
